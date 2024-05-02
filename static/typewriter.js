1f6f7d2264d11f0409b1d5bc7975ce9f6b268a20
Andrei Adlawan <andreigabrielle.adlawan@g.msuiit.edu.ph>
2024-05-02 08:43:11 +0800

    first commit

---
+++ static/typewriter.js
@@ -0,0 +1,30 @@
+const textArray = ["Custom Emotes", "Illustrations", "Twitch Panels","PNGTuber Models","Sub Badges"];
+let index = 0;
+let currentText = "";
+let letterIndex = 0;
+
+function type() {
+    if (letterIndex < textArray[index].length) {
+        currentText += textArray[index].charAt(letterIndex);
+        document.getElementById("typewriter").innerHTML = currentText;
+        letterIndex++;
+        setTimeout(type, 100); // Adjust typing speed (milliseconds)
+    } else {
+        setTimeout(erase, 1200); // Wait before erasing
+    }
+}
+
+function erase() {
+    if (letterIndex >= 0) {
+        currentText = textArray[index].substring(0, letterIndex);
+        document.getElementById("typewriter").innerHTML = currentText;
+        letterIndex--;
+        setTimeout(erase, 50); // Adjust erasing speed (milliseconds)
+    } else {
+        index = (index + 1) % textArray.length;
+        setTimeout(type, 500); // Wait before typing the next text
+    }
+}
+
+// Start the typing effect
+type();
